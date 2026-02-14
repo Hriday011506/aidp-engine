85ec10fe1d3f43eefef8c281d2a0b40bc4737113
Hriday Mahajan <hridaymahajan1979@gmail.com>
2026-02-14 14:13:24 +0530

    Implement demand prediction function
    Add a function to predict demand using a pre-trained model.

--- src/predict.py
+++ src/predict.py
@@ -1,1 +1,13 @@
+import pickle
+import numpy as np
 
+def predict_demand(avg_temp, local_events, viral_score):
+
+    with open("models/demand_model.pkl", "rb") as f:
+        model = pickle.load(f)
+
+    prediction = model.predict(
+        np.array([[avg_temp, local_events, viral_score]])
+    )[0]
+
+    return prediction
